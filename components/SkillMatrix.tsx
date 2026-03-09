1cb66762da94e97b6946ee3b3dee34fc30db8db6
manish9427 <manish119427@gmail.com>
2026-03-09 22:55:01 +0530

    new

--- components/SkillMatrix.tsx
+++ components/SkillMatrix.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect, useRef, useMemo } from "react";
+import { useState, useEffect, useRef } from "react";
 import * as d3 from "d3";
 
 type ProficiencyLevel = "learning" | "familiar" | "expert";
